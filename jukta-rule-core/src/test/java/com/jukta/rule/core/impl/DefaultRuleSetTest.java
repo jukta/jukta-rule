9a8641dca934f60d8157941181834b5f50f0afa7
MarishkaKravchenko <jellybee.droid@gmail.com>
2017-03-04 21:59:47 +0200

    add number predicates

--- jukta-rule-core/src/test/java/com/jukta/rule/core/impl/DefaultRuleSetTest.java
+++ jukta-rule-core/src/test/java/com/jukta/rule/core/impl/DefaultRuleSetTest.java
@@ -3,6 +3,7 @@ package com.jukta.rule.core.impl;
 import com.jukta.rule.core.builder.RuleBuilder;
 import com.jukta.rule.core.builder.RuleSetBuilder;
 import com.jukta.rule.core.predicate.AnyPredicate;
+import com.jukta.rule.core.predicate.NumberPredicates.*;
 import com.jukta.rule.core.predicate.StringPredicate;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +23,12 @@ public class DefaultRuleSetTest {
                 .singleRuleSet("test", String[].class, String.class)
                 .addField("f1", o -> o[0], 1)
                 .addField("f2", o -> o[1], 0)
+                .addField("f3", o -> o[2], 0)
+                .addField("f4", o -> o[3], 0)
+                .addField("f5", o -> o[4], 0)
+                .addField("f6", o -> o[5], 0)
+                .addField("f7", o -> o[6], 0)
+                .addField("f8", o -> o[7], 0)
                 .build();
     }
 
@@ -31,16 +38,28 @@ public class DefaultRuleSetTest {
         RuleBuilder.rule("", ruleSet)
                 .addPredicate("f1", new AnyPredicate())
                 .addPredicate("f2", new StringPredicate("a"))
+                .addPredicate("f3", new NumberEqualsPredicate("1"))
+                .addPredicate("f4", new NumberGreaterPredicate("1"))
+                .addPredicate("f5", new NumberLessPredicate("2"))
+                .addPredicate("f6", new NumberLessOrEqualsPredicate("1"))
+                .addPredicate("f7", new NumberNotEqualsPredicate("1"))
+                .addPredicate("f8", new NumberGreaterOrEqualsPredicate("1"))
                 .setResultFactory((strings, o) -> "a")
                 .build();
 
         RuleBuilder.rule("", ruleSet)
                 .addPredicate("f1", new AnyPredicate())
                 .addPredicate("f2", new StringPredicate("b"))
+                .addPredicate("f3", new NumberEqualsPredicate("1"))
+                .addPredicate("f4", new NumberGreaterPredicate("2"))
+                .addPredicate("f4", new NumberLessPredicate("2"))
+                .addPredicate("f4", new NumberLessOrEqualsPredicate("2"))
+                .addPredicate("f4", new NumberNotEqualsPredicate("2"))
+                .addPredicate("f4", new NumberGreaterOrEqualsPredicate("2"))
                 .setResultFactory((strings, o) -> "b")
                 .build();
 
-        assertEquals(ruleSet.eval(new String[]{"", "a"}), "a");
+        assertEquals(ruleSet.eval(new String[]{"", "a", "1", "2","1","1","2","2"}), "a");
     }
 
     @Test
@@ -49,16 +68,28 @@ public class DefaultRuleSetTest {
         RuleBuilder.rule("", ruleSet)
                 .addPredicate("f1", new AnyPredicate())
                 .addPredicate("f2", new AnyPredicate())
+                .addPredicate("f3", new AnyPredicate())
+                .addPredicate("f4", new AnyPredicate())
+                .addPredicate("f5", new AnyPredicate())
+                .addPredicate("f6", new AnyPredicate())
+                .addPredicate("f7", new AnyPredicate())
+                .addPredicate("f8", new AnyPredicate())
                 .setResultFactory((strings, o) -> "a")
                 .build();
 
         RuleBuilder.rule("", ruleSet)
                 .addPredicate("f1", new StringPredicate("a"))
                 .addPredicate("f2", new AnyPredicate())
+                .addPredicate("f3", new AnyPredicate())
+                .addPredicate("f4", new AnyPredicate())
+                .addPredicate("f5", new AnyPredicate())
+                .addPredicate("f6", new AnyPredicate())
+                .addPredicate("f7", new AnyPredicate())
+                .addPredicate("f8", new AnyPredicate())
                 .setResultFactory((strings, o) -> "b")
                 .build();
 
-        assertEquals(ruleSet.eval(new String[]{"a", "a"}), "b");
+        assertEquals(ruleSet.eval(new String[]{"a", "a", "a", "a", "a", "a", "a", "a"}), "b");
     }
 
     @Test
@@ -67,16 +98,28 @@ public class DefaultRuleSetTest {
         RuleBuilder.rule("", ruleSet)
                 .addPredicate("f1", new AnyPredicate())
                 .addPredicate("f2", new StringPredicate("a"))
+                .addPredicate("f3", new AnyPredicate())
+                .addPredicate("f4", new AnyPredicate())
+                .addPredicate("f5", new AnyPredicate())
+                .addPredicate("f6", new AnyPredicate())
+                .addPredicate("f7", new AnyPredicate())
+                .addPredicate("f8", new AnyPredicate())
                 .setResultFactory((strings, o) -> "a")
                 .build();
 
         RuleBuilder.rule("", ruleSet)
                 .addPredicate("f1", new StringPredicate("a"))
                 .addPredicate("f2", new AnyPredicate())
+                .addPredicate("f3", new AnyPredicate())
+                .addPredicate("f4", new AnyPredicate())
+                .addPredicate("f5", new AnyPredicate())
+                .addPredicate("f6", new AnyPredicate())
+                .addPredicate("f7", new AnyPredicate())
+                .addPredicate("f8", new AnyPredicate())
                 .setResultFactory((strings, o) -> "b")
                 .build();
 
-        assertEquals(ruleSet.eval(new String[]{"a", "a"}), "b");
+        assertEquals(ruleSet.eval(new String[]{"a","a","","","","","",""}), "b");
     }
 
     @Test(expected = RuntimeException.class)
@@ -103,11 +146,16 @@ public class DefaultRuleSetTest {
         RuleBuilder.rule("", ruleSet)
                 .addPredicate("f1", new AnyPredicate())
                 .addPredicate("f2", new AnyPredicate())
+                .addPredicate("f3", new AnyPredicate())
+                .addPredicate("f4", new AnyPredicate())
+                .addPredicate("f5", new AnyPredicate())
+                .addPredicate("f6", new AnyPredicate())
+                .addPredicate("f7", new AnyPredicate())
+                .addPredicate("f8", new AnyPredicate())
                 .setResultFactory((strings, o) -> o + "a")
                 .build();
 
         ruleSet.setInitialFactory((strings, o) -> "A");
-        assertEquals(ruleSet.eval(new String[]{"a", "a"}), "Aa");
+        assertEquals(ruleSet.eval(new String[]{"a","a","","","","","",""}), "Aa");
     }
-
 }
